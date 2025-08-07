9b9b72ce7ce03e1f80b0ac8258d2acc89343b65d
Rifle Bird <jumislt@gmail.com>
2025-08-07 19:55:34 +0300

    Added basic header

---
+++ script.js
@@ -0,0 +1,14 @@
+/* Hello and welcome to the hellscape that is javascript!
+Behold, but don't be scared as we descend into madness... */
+
+header.innerHTML = `
+<header id="header" class="header">
+    <div class="logo">
+        <img src="/assets/blue-sprite2.png">
+    </div>
+    <nav>   
+        <a href="/not_found.html">About</a>
+        <a href="/not_found.html">Contact</a>
+    </nav>
+</header>
+`;
