412e4271b93ca3b109f7b58a16f8e8384cd40047
Rifle Bird <jumislt@gmail.com>
2025-08-08 10:14:48 +0300

    Minor style adjustments

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Code for my neocities website: https://riflebird.neocities.org/
+---
+Resources used:
+https://www.joshwcomeau.com/css/custom-css-reset/
--- script.js
+++ script.js
@@ -4,7 +4,7 @@ Behold, but don't be scared as we descend into madness... */
 header.innerHTML = `
 <header id="header" class="header">
     <div class="logo">
-        <img src="/assets/blue-sprite2.png">
+        <img src="/assets/blue-sprite2.png" alt="logo">
     </div>
     <nav>   
         <a href="/not_found.html">About</a>
