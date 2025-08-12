171099b715f53d187d834fcbb6e7d897c17ee637
Rifle Bird <jumislt@gmail.com>
2025-08-12 16:13:54 +0300

    Added content box

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /* Hello and welcome to the hellscape that is javascript!
 Behold, but don't be scared as we descend into madness... */
 
+// Header
 header.innerHTML = `
 <header id="header" class="header">
     <div class="logo">
@@ -13,9 +14,18 @@ header.innerHTML = `
 </header>
 `;
 
+// Footer
 footer.innerHTML = `
 <footer class="footer">
     <p>© 2025 riflebird</p>
     <p>Powered by <a href="https://neocities.org">Neocities</a></p>
 </footer>
 `
+
+// Markdown text thingamajigger
+// const mdURL = 'https://res.cloudinary.com/dqes5rgqn/raw/upload/v1754563106/README_rc2k9m.md';
+// fetch(mdURL).then(function(response) {
+//     return response.text();
+// }).then(function(markdown){
+//     document.getElementById("test").innerHTML = marked.parse(markdown);
+// })
