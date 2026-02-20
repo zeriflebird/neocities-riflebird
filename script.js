2d3d361ed73eb7ba6fffc9ee306791506e5c2bb5
Rifle Bird <jumislt@gmail.com>
2026-02-20 18:58:18 +0200

    Style and info update

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ Behold, but don't be scared as we descend into madness... */
 // Header
 header.innerHTML = `
 <header id="header" class="header">
-    <div class="logo">
-        <img src="/assets/blue-sprite2.png" alt="logo">
-    </div>
+    
     <nav>   
+        <p class="thick">riflebird</p>
+        <p>.neocities.com</p>
         <a href="/not_found.html">About</a>
+        <a href="https://cara.app/riflebird">Gallery</a>
         <a href="/not_found.html">Contact</a>
     </nav>
 </header>
@@ -17,15 +18,7 @@ header.innerHTML = `
 // Footer
 footer.innerHTML = `
 <footer class="footer">
-    <p>© 2025 riflebird</p>
     <p>Powered by <a href="https://neocities.org">Neocities</a></p>
 </footer>
 `
 
-// Markdown text thingamajigger
-// const mdURL = 'https://res.cloudinary.com/dqes5rgqn/raw/upload/v1754563106/README_rc2k9m.md';
-// fetch(mdURL).then(function(response) {
-//     return response.text();
-// }).then(function(markdown){
-//     document.getElementById("test").innerHTML = marked.parse(markdown);
-// })
